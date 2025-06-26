5d51ebf14a6aaf595baf653fc389156a0a19981d
Stefano Casasola <ste.casasola@gmail.com>
2025-06-26 13:16:02 +0200

    Finishes the lecture

--- script/animation.js
+++ script/animation.js
@@ -1,2 +1,14 @@
 // dovremo recuperare il bottone "start"
+const startButton = document.getElementById('start')
 // e alla sua pressione, applicare al box "go" la classe "moving"
+startButton.addEventListener('click', () => {
+  // cercare il box sotto al mottone
+  const boxToMove = document.getElementById('go')
+  // aggiungiamoci la classe "moving"
+  boxToMove.classList.add('moving')
+  //   faccio anche in modo di "resettare" l'elemento una volta finita l'animazione
+  setTimeout(() => {
+    // dopo 3 secondi... tolgo la classe "moving"
+    boxToMove.classList.remove('moving')
+  }, 3000)
+})
